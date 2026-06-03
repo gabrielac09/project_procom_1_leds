72cd50b5f90bbf781fa70fb9749fcc2f75c91fb7
Gabriela Contreras <gabrielac0901@gmail.com>
2026-06-03 10:28:10 -0300

    Actualizacion del top

--- project_procom_1_leds.srcs/sources_1/new/top.v
+++ project_procom_1_leds.srcs/sources_1/new/top.v
@@ -20,31 +20,51 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module top(                                                                             
-     output [3 :0] o_led  ,                             
-     output [3 :0] o_led_b  ,                           
-     output [3 :0] o_led_g  ,                             
-     input  [3 :0]  i_sw   ,                             
-     input        i_reset,                             
+module top
+#(
+     parameter NB_SW = 4 ,          
+     parameter NB_COUNTER = 32,
+     parameter NB_LEDS = 4
+)
+(                                                                             
+     output [NB_LEDS-1:0] o_led  ,                             
+     output [NB_LEDS-1:0] o_led_b  ,                           
+     output [NB_LEDS-1:0] o_led_g  ,   
+
+     input  [NB_SW-1:0]  i_sw   ,                             
+     input       i_reset,                             
      input       clock                                                                             
 );
 
 count
+#(
+    .NB_SW       (NB_SW - 1),
+    .NB_COUNTER  (NB_COUNTER)
+)
      ucount(
-     .o_valid (w_valid),
+     .o_valid (connect_valid),
      .i_sw    (i_sw[2:0]),
-     .i_reset (i_reset),
+     .i_reset (~i_reset),
      .clock   (clock)                         
     );
 
 shiftreg
+#(
+    .NB_LEDS (NB_LEDS)
+)
      ushiftreg(
-     .o_led  (w_led),
-     .i_valid(w_valid),
-     .i_reset(i_reset),
+     .o_led  (connect_leds),
+     .i_valid(connect_valid),
+     .i_reset(~i_reset),
      .clock   (clock)                         
     );
 
+wire [NB_LEDS-1:0] connect_leds;
+wire               connect_valid;
 
+//con esto es azul o verde dependiendo del valor de i_sw[3]
+assign o_led = connect_led;
+assign o_led_b = (i_sw[3]==2'b00) ? connect_leds : {NB_LEDS{1'b0}};
+assign o_led_g = (i_sw[3]==2'b00) ? {NB_LEDS{1'b0}} : connect_leds; //este es Not del azul
 
 endmodule
